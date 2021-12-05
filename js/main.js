4c0cecd30ee7315bfd746036dee854586fe6ca7e
Keshav Pandey <keshav25@outlook.com>
2021-12-04 16:25:44 -0800

    Update site

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-/**
-* Template Name: Butterfly - v4.7.0
-* Template URL: https://bootstrapmade.com/butterfly-free-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+function sendMail()
+{
+    var yourMessage = document.getElementById("message").value;
+    var subject = document.getElementById("subject").value;
+    document.location.href = "mailto:glacierrmangement@gmail.com?subject="
+        + encodeURIComponent(subject)
+        + "&body=" + encodeURIComponent(yourMessage);
+}
+
 (function() {
   "use strict";
 
@@ -74,6 +77,13 @@
     })
   }
 
+    /**
+   * Initiate glightbox 
+   */
+     const glightbox = GLightbox({
+      selector: '.glightbox'
+    });
+    
   /**
    * Back to top button
    */
@@ -136,91 +146,6 @@
         scrollto(window.location.hash)
       }
     }
-  });
-
-  /**
-   * Initiate glightbox 
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
-
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-
-      }, true);
-    }
-
-  });
-
-  /**
-   * Initiate portfolio lightbox 
-   */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
-   * Initiate galleery lightbox 
-   */
-  const galleeryLightbox = GLightbox({
-    selector: '.galleery-lightbox'
   });
 
 })()
